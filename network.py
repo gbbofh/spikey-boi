74c2675e2f340a3f94215c4f4563f0a228666726
Garrett Bates <garrett.bates@student.nmt.edu>
2019-11-22 15:38:57 -0700

    Removed dead code and updated comments

--- network.py
+++ network.py
@@ -8,6 +8,7 @@ def isin(el, te, au=False, invert=False):
 
 class Network():
 
+    # Constants used for STDP
     STDP_INIT = 0.2
     STDP_DECAY = 0.2
     SYNAPSE_DECAY = 0.0001
@@ -43,6 +44,11 @@ class Network():
         self.reset = numpy.ones(totalNum)
         self.uReset = numpy.ones(totalNum)
 
+        # Initialize neuron parameters.
+        # This will create both excitatory and inhibitory neurons
+        # as well as neurons with various types of behaviours. Neuron
+        # behaviours are biased towards regular spiking, but allow for
+        # chattering, fast spiking, and other behaviours
         self.scale[0 : numEx] *= 0.02
         self.scale[numEx : ] *= 0.02 + 0.08 * r[numEx : ]
         self.uSens[0 : numEx] *= 0.2
@@ -83,10 +89,16 @@ class Network():
         self.voltage[spikes] = self.reset[spikes]
         self.recovery[spikes] += self.uReset[spikes]
 
+        # Add the sensory input to the input vector
+        # It is assumed that the input vector is cleared/reset
+        # OUTSIDE of the network. This may change in the future
+        # but right now stochastic inputs are generated in agent.py
         self.input[self.sensorySyn] += self.sensoryInput
-        #self.input[self.sensorySyn] = self.sensoryInput
         self.input += self.synapses[spikes, :].sum(axis=0)
 
+        # Calculate two iterations for stability
+        # This equation comes from Izhikevich "Simple Model of Spiking Neurons"
+        # published in 2003.
         self.voltage += 0.5 * (0.04 * self.voltage ** 2 +
                        5.0 * self.voltage + 140 - self.recovery + self.input)
         self.voltage += 0.5 * (0.04 * self.voltage ** 2 +
@@ -95,13 +107,29 @@ class Network():
 
         self.voltage[numpy.where(self.voltage >= 30.0)] = 30.0
 
+
+        # Accumulate motor outputs -- this is a wee bit iffy. I was
+        # experimenting with different methods that could be used to determine
+        # the motor output, and this seemed to be the most reliable. This will
+        # probably get moved to Agent.update
         mo = numpy.where(self.voltage[self.motorSyn] >= 30.0, True, False)
 
         self.motorOutput = numpy.clip(mo - 0.1 * self.motorOutput, 0.0, 1.0)
 
+
+        # Calculate the next iteration of the decaying STDP constants for
+        # neurons which have already fired
         self.stdp *= Network.STDP_DECAY
+
+        # For any neurons that just spiked, setup the STDP values
         self.stdp[spikes] = Network.STDP_INIT
 
+        # Iterate over all synaptic weights in order to update the weight
+        # matrix. I feel like there is a better way to do this through numpy,
+        # but I couldn't find anything online about it and most suggestions were
+        # to either do what I am doing here, or to generate a list of all
+        # possible indices... This seemed like the faster solution to get this
+        # prototype running.
         for s in range(self.totalNum):
             for ps in range(self.totalNum):
                 # We have to check if it is less than STDP_INIT in order to
@@ -117,18 +145,7 @@ class Network():
                     # w' = w(1 - k)
                     # w = e^(t - kt)
                     self.synapses[ps, s] -= Network.SYNAPSE_DECAY * self.synapses[ps, s]
-                # if s in spikes and self.synapses[ps, s] != 0.0:
-                    #self.synapses[ps, s] += self.stdp[ps] * 0.9
-                    #self.stdp[ps] = 0.0
-                #else:
-                    #self.synapses[ps, s] -= 0.0005 * self.synapses[ps, s]
 
-        # TODO: FIX THIS BULLSHIT. There has to be a better way.
-        # for s in range(self.totalNum):
-        #     for ps in range(self.totalNum):
-        #         if s in spikes:
-        #             self.stdp[ps] = 0.0
-        # Lock weights to be between -1.0 and 1.0
         self.synapses = numpy.clip(self.synapses, -1.0, 1.0)
 
         self.pSpikes = spikes
