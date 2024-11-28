1a8f2861c6411e8d9b42bfc0a8ca06896265134e
Garrett Bates <garrett.bates@alumni.nmt.edu>
2024-11-27 22:04:41 -0700

    Flipped pixels of the display to match agent vision cone.

--- spikeyboi/ui/agent_vision.py
+++ spikeyboi/ui/agent_vision.py
@@ -16,22 +16,28 @@ class UIAgentVisionDebugger(spikeyboi.ui.debug_window.UIDebugWindow):
 
         # First 7 inputs are raycast results
         self.buffer = pg.Surface((self.brain.num_inputs - 3, self.brain.num_inputs - 3), pg.SRCALPHA)
-        self.kernel = np.array([
-            [ 0.25, 0.25, ],
-            [ 0.25, 0.25, ],
-        ])
+        # self.kernel = np.array([
+        #     [ 0.25, 0.25, ],
+        #     [ 0.25, 0.25, ],
+        # ])
 
-        self.kernel_enabled = True
+        # self.kernel_enabled = True
         self.data = np.zeros((self.brain.num_inputs - 3, self.brain.num_inputs - 3), dtype=np.float64)
         self.alpha = 1.0
 
         spikeyboi.app_instance.on_agent_selected_event.append(self.on_agent_selected)
 
     def on_update(self, delta_time):
-        self.data[:] = self.brain.inputs[:7, np.newaxis]
+        self.data[:] = np.flip(self.brain.inputs[:7])[:,np.newaxis]
         values = self.data
 
         rgba = spikeyboi.ui.colormaps['zebra'](values)
+
+        # if not (self.kernel is None) and self.kernel_enabled:
+        #     rgba[:,:,0] = sp.ndimage.convolve(rgba[:,:,0], self.kernel)
+        #     rgba[:,:,1] = sp.ndimage.convolve(rgba[:,:,1], self.kernel)
+        #     rgba[:,:,2] = sp.ndimage.convolve(rgba[:,:,2], self.kernel)
+        #     rgba[:,:,3] = sp.ndimage.convolve(rgba[:,:,3], self.kernel)
 
         self.buffer.fill((0,0,0,255))
         pg.surfarray.blit_array(self.buffer, rgba[:,:,:-1])
