3d2ecb04d98d10af28292738d5dc2e4bc93dee36
Garrett Bates <garrett.bates@alumni.nmt.edu>
2024-11-27 22:05:37 -0700

    major rework of agent vision to cast more rays and find the average value of the result

--- spikeyboi/spikey/agent.py
+++ spikeyboi/spikey/agent.py
@@ -35,31 +35,42 @@ class Agent(pg.sprite.Sprite):
         # self.brain = spikeyboi.spikey.brain.Brain(50, 7, 2)
         self.brain = spikeyboi.spikey.brain.Brain(30, 10, 2)
         self.on_agent_moved_event = []
-        self.ray_angles = np.array([
-            np.pi / 3, 
-            np.pi / 6,
-            np.pi / 12,
-            0,
-            -np.pi / 12,
-            -np.pi / 6,
-            -np.pi / 3
-        ])
+        # self.ray_angles = np.array([
+        #     np.pi / 3, 
+        #     np.pi / 6,
+        #     np.pi / 12,
+        #     0,
+        #     -np.pi / 12,
+        #     -np.pi / 6,
+        #     -np.pi / 3
+        # ])
 
-        self.ray_angles[:] /= 2
-        self.ray_angles[1:-1] /= 2
+        # self.ray_angles[:] /= 2
+        # self.ray_angles[1:-1] /= 2
+        self.angles = np.linspace(-np.pi / 3, np.pi / 3, 20)
+        self.angles[:] /= 4
+
+        self.inputs = np.zeros_like(self.angles)
 
         # For reward modulation
-        self.prev_x = self.x
-        self.prev_y = self.y
+        # self.prev_x = self.x
+        # self.prev_y = self.y
 
-        self.distances = np.zeros_like(self.ray_angles)
-        self.prev_distances = np.zeros_like(self.ray_angles)
+        # self.distances = np.zeros_like(self.ray_angles)
+        # self.prev_distances = np.zeros_like(self.ray_angles)
 
-        self.hits = []
-        self.input_scale = np.zeros_like(self.brain.inputs)
-        self.sigmoid_scale = np.zeros_like(self.brain.inputs)
+        self.distances = np.zeros_like(self.angles)
+        self.prev_distances = np.zeros_like(self.angles)
+        self.deltas = np.zeros_like(self.brain.inputs[:7])
 
-        self.S = lambda x,s: 0.5 / (1 + np.exp(s * (x - 0.8)))
+        self.mean_distances = np.zeros_like(self.brain.inputs[:7])
+
+        # self.hits = []
+        # self.input_scale = np.zeros_like(self.brain.inputs)
+        # self.sigmoid_scale = np.zeros_like(self.brain.inputs)
+
+        # self.S = lambda x,s: 0.5 / (1 + np.exp(s * (x - 0.8)))
+        self.S = lambda x,s: 0.5 / (1 + np.exp(s * (x - 1.0)))
         self.id = 0
         self.food_consumed = 0
 
@@ -79,66 +90,116 @@ class Agent(pg.sprite.Sprite):
     def fixed_update(self, fixed_delta):
         sim = spikeyboi.spikey.sim_instance
 
-        self.distances[:] = 0
-        self.hits.clear()
+        # self.distances[:] = 0
+        # self.hits.clear()
 
-        for i,a in enumerate(self.ray_angles):
-            angle_plus = self.angle + a
-            dir_plus = np.array((np.cos(angle_plus), -np.sin(angle_plus)))
+        # angles = np.linspace(-np.pi / 3, np.pi / 3, 20)
+        # angles[:] /= 4
 
-            center = self.rect.center
+        # inputs = np.zeros_like(angles)
 
-            ray_length = np.max(spikeyboi.spikey.sim_instance.size)
+        for i, a in enumerate(self.angles):
+            angle = self.angle + a
+            dir = np.array((np.cos(angle), -np.sin(angle)))
 
-            # TODO: Eventually rework this to use cast cone?
-            # hit = sim.physics.cast_cone(center, dir_plus, np.pi/6, ray_length, exclude={self})
-            hit = sim.physics.cast_ray(center, dir_plus, ray_length, exclude={self})
-            self.hits.append(hit)
-
+            origin = self.rect.center
+            dist = np.max(spikeyboi.spikey.sim_instance.size)
+            hit = sim.physics.cast_ray(origin, dir, dist, exclude={self})
             if hit:
-                obj, point, dist = hit
-                max_dist = np.linalg.norm(spikeyboi.spikey.sim_instance.size)
-                # self.distances[i] = dist
-                self.distances[i] = dist / max_dist
-                if type(obj) is spikeyboi.spikey.food.Food:
-                    self.input_scale[i] = 5
-                    self.sigmoid_scale[i] = 0.8
-                    self.brain.rewards[i,:] += 0.06 * fixed_delta
+                obj, point, hit_dist = hit
 
-                    presyn = self.brain.net.P_pre > 0.2
-                    self.brain.rewards[presyn] += 0.02 * fixed_delta
+                m = {
+                    spikeyboi.spikey.food.Food: 5,
+                    spikeyboi.spikey.agent.Agent: 4,
+                    spikeyboi.spikey.wall.Wall: 3,
+                }
 
-                    postsyn = self.brain.net.P_post < 0.01
-                    self.brain.rewards[postsyn] -= 0.01 * fixed_delta
+                n = {
+                    spikeyboi.spikey.food.Food: 0.8,
+                    spikeyboi.spikey.agent.Agent: 0.6,
+                    spikeyboi.spikey.wall.Wall: 0.4,
+                }
 
-                elif type(obj) is spikeyboi.spikey.agent.Agent:
-                    self.input_scale[i] = 4
-                    self.sigmoid_scale[i] = 0.6
-                    self.brain.rewards[i,:] += 0.04 * fixed_delta
+                scale = n[type(obj)]
+                input = m[type(obj)]
+                self.inputs[i] = scale * self.S(hit_dist / dist, input)
 
-                    presyn = self.brain.net.P_pre > 0.2
-                    self.brain.rewards[presyn] += 0.02 * fixed_delta
+        left = self.inputs[:5].mean()
+        right = self.inputs[-5:].mean()
 
-                    postsyn = self.brain.net.P_post < 0.01
-                    self.brain.rewards[postsyn] -= 0.01 * fixed_delta
+        center = self.inputs[5:-5].reshape(5,2).mean(axis=1)
 
-                elif type(obj) is spikeyboi.spikey.wall.Wall:
-                    self.input_scale[i] = 3
-                    self.sigmoid_scale[i] = 0.4
-                    self.brain.rewards[i,:] -= 0.02 * fixed_delta
+        vision = self.brain.inputs[:7]
 
-                    presyn = self.brain.net.P_pre > 0.2
-                    presyn = presyn[i]
-                    self.brain.rewards[presyn,i] -= 0.02 * fixed_delta
+        vision[0] = left
+        vision[1:-1] = center
+        vision[-1] = right
 
-                    postsyn = self.brain.net.P_post < 0.01
-                    postsyn = postsyn[i]
-                    self.brain.rewards[postsyn,i] -= 0.02 * fixed_delta
+        R = self.brain.rewards[:7]
+        R[vision > 0.03, :] += 0.02 * fixed_delta
+        R[vision > 0.08,:] += 0.02 * fixed_delta
+        R[vision > 0.15,:] += 0.02 * fixed_delta
 
-        deltas = self.distances - self.prev_distances
-        self.prev_distances = self.distances
+        presyn = self.brain.net.P_pre > 0.3
+        postsyn = self.brain.net.P_pre < 0.2
 
-        self.brain.inputs[:7] = self.sigmoid_scale[:7] * self.S(self.distances, self.input_scale[:7])
+        self.brain.net.reward[presyn, :] += 0.02 * fixed_delta
+        self.brain.net.reward[postsyn, :] -= 0.01 * fixed_delta
+
+        # for i,a in enumerate(self.ray_angles):
+        #     angle_plus = self.angle + a
+        #     dir_plus = np.array((np.cos(angle_plus), -np.sin(angle_plus)))
+
+        #     center = self.rect.center
+
+        #     ray_length = np.max(spikeyboi.spikey.sim_instance.size)
+
+        #     # TODO: Eventually rework this to use cast cone?
+        #     # hit = sim.physics.cast_cone(center, dir_plus, np.pi/6, ray_length, exclude={self})
+        #     hit = sim.physics.cast_ray(center, dir_plus, ray_length, exclude={self})
+        #     self.hits.append(hit)
+
+        #     if hit:
+        #         obj, point, dist = hit
+        #         max_dist = np.linalg.norm(spikeyboi.spikey.sim_instance.size)
+        #         # self.distances[i] = dist
+        #         self.distances[i] = dist / max_dist
+        #         if type(obj) is spikeyboi.spikey.food.Food:
+        #             self.input_scale[i] = 5
+        #             self.sigmoid_scale[i] = 0.8
+        #             self.brain.rewards[i,:] += 0.06 * fixed_delta
+
+        #             presyn = self.brain.net.P_pre > 0.2
+        #             self.brain.rewards[presyn] += 0.02 * fixed_delta
+
+        #             postsyn = self.brain.net.P_post < 0.01
+        #             self.brain.rewards[postsyn] -= 0.01 * fixed_delta
+
+        #         elif type(obj) is spikeyboi.spikey.agent.Agent:
+        #             self.input_scale[i] = 4
+        #             self.sigmoid_scale[i] = 0.6
+        #             self.brain.rewards[i,:] += 0.04 * fixed_delta
+
+        #             presyn = self.brain.net.P_pre > 0.2
+        #             self.brain.rewards[presyn] += 0.02 * fixed_delta
+
+        #             postsyn = self.brain.net.P_post < 0.01
+        #             self.brain.rewards[postsyn] -= 0.01 * fixed_delta
+
+        #         elif type(obj) is spikeyboi.spikey.wall.Wall:
+        #             self.input_scale[i] = 3
+        #             self.sigmoid_scale[i] = 0.4
+        #             self.brain.rewards[i,:] -= 0.02 * fixed_delta
+
+        #             presyn = self.brain.net.P_pre > 0.2
+        #             presyn = presyn[i]
+        #             self.brain.rewards[presyn,i] -= 0.02 * fixed_delta
+
+        #             postsyn = self.brain.net.P_post < 0.01
+        #             postsyn = postsyn[i]
+        #             self.brain.rewards[postsyn,i] -= 0.02 * fixed_delta
+
+        # self.brain.inputs[:7] = self.sigmoid_scale[:7] * self.S(self.distances, self.input_scale[:7])
         if self.hit_wall:
             self.hit_wall = False
 
@@ -167,10 +228,10 @@ class Agent(pg.sprite.Sprite):
                     self.brain.inputs[Agent.WALL_HIT_R_ID] = 1.0
                     self.brain.rewards[Agent.WALL_HIT_R_ID] += 0.002
 
-            mask = self.distances > 0
-            r = self.brain.rewards[:len(self.distances)]
+            # mask = self.distances > 0
+            # r = self.brain.rewards[:len(self.distances)]
 
-            r[mask, :] += 0.1 * spikeyboi.spikey.sim_instance.fixed_delta_time
+            # r[mask, :] += 0.1 * spikeyboi.spikey.sim_instance.fixed_delta_time
 
             self.brain.rewards[:self.brain.net.num_exc,ltp] += 0.1 * spikeyboi.spikey.sim_instance.fixed_delta_time
             self.brain.rewards[self.brain.net.num_exc:,ltp] -= 0.2 * spikeyboi.spikey.sim_instance.fixed_delta_time
@@ -193,10 +254,31 @@ class Agent(pg.sprite.Sprite):
 
         self.brain.inputs[:] *= np.exp(-1/20)
 
-        inds = np.where(deltas > 0)
+        deltas = self.distances - self.prev_distances
+        self.prev_distances = self.distances
+
+        self.mean_distances[0] = self.distances[:5].mean()
+        self.mean_distances[1:-1] = self.distances[5:-5].mean()
+        self.mean_distances[-1] = self.distances[-5:].mean()
+
+        left = deltas[:5].mean()
+        center = deltas[5:-5].mean()
+        right = deltas[-5:].mean()
+
+        self.deltas[0] = left
+        self.deltas[1:-1] = center
+        self.deltas[-1] = right
+
+        # self.brain.rewards[:, self.deltas > 0] += 0.06 * fixed_delta
+        inds = np.where(self.deltas > 0)
         for i in np.nditer(inds, ('zerosize_ok',)):
             self.brain.rewards[:,i] += 0.08 * fixed_delta
         self.brain.update(fixed_delta)
+
+        # inds = np.where(deltas > 0)
+        # for i in np.nditer(inds, ('zerosize_ok',)):
+        #     self.brain.rewards[:,i] += 0.08 * fixed_delta
+        # self.brain.update(fixed_delta)
 
         self.angle += 0.2 * self.brain.outputs[0] * fixed_delta
         self.angle -= 0.2 * self.brain.outputs[1] * fixed_delta
